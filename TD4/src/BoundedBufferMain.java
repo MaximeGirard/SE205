260c60ec94dac49472c92c85193408c1a7b868d3
Maxime GIRARD <maxime.girard@telecom-paris.fr>
2024-03-11 11:38:20 +0100

    TD4 / Synchronization using native Java functionnalities

--- TD4/src/BoundedBufferMain.java
+++ TD4/src/BoundedBufferMain.java
@@ -1,18 +1,18 @@
 public class BoundedBufferMain {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         BoundedBuffer buffer;
 
         // Check the arguments of the command line
-        if (args.length != 1){
-            System.out.println ("PROGRAM FILENAME");
+        if (args.length != 1) {
+            System.out.println("PROGRAM FILENAME");
             System.exit(1);
         }
         Utils.init(args[0]);
 
-        Consumer consumers[] = new Consumer[(int)Utils.nConsumers];
-        Producer producers[] = new Producer[(int)Utils.nProducers];
-        
+        Consumer consumers[] = new Consumer[(int) Utils.nConsumers];
+        Producer producers[] = new Producer[(int) Utils.nProducers];
+
         // Create a buffer
         if (Utils.sem_impl == 0)
             buffer = new NatBoundedBuffer(Utils.bufferSize);
@@ -20,8 +20,34 @@ public class BoundedBufferMain {
             buffer = new SemBoundedBuffer(Utils.bufferSize);
 
         // Create producers and then consumers
-        // Create consumers
+        // Create producers
+        for (int i = 0; i < Utils.nProducers; i++) {
+            producers[i] = new Producer(i, buffer);
+            producers[i].start();
+        }
 
-// Create producers
+        // Create consumers
+        for (int i = 0; i < Utils.nConsumers; i++) {
+            consumers[i] = new Consumer(i, buffer);
+            consumers[i].start();
+        }
+
+        // Wait for the end of the producers
+        for (int i = 0; i < Utils.nProducers; i++) {
+            try {
+                producers[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Wait for the end of the consumers
+        for (int i = 0; i < Utils.nConsumers; i++) {
+            try {
+                consumers[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- TD4/src/NatBoundedBuffer.java
+++ TD4/src/NatBoundedBuffer.java
@@ -4,110 +4,157 @@ import java.util.concurrent.TimeUnit;
 
 class NatBoundedBuffer extends BoundedBuffer {
 
-    // Initialise the protected buffer structure above. 
-    NatBoundedBuffer (int maxSize) {
-        super(maxSize);
-    }
+   // Initialise the protected buffer structure above.
+   NatBoundedBuffer(int maxSize) {
+      super(maxSize);
+   }
 
-    // Extract an element from buffer. If the attempted operation is
-    // not possible immediately, the method call blocks until it is.
-    Object get() {
-       Object value;
+   // Extract an element from buffer. If the attempted operation is
+   // not possible immediately, the method call blocks until it is.
+   synchronized Object get() {
+      // Enter mutual exclusion done with synchronized -> taking the lock
+      Object value;
 
-       // Enter mutual exclusion
-            
-          // Wait until there is a full slot available.
+      // Wait until there is a full slot available.
+      while (size == 0) {
+         try {
+            wait();
+         } catch (InterruptedException e) {
+            e.printStackTrace();
+         }
+      }
 
-          // Signal or broadcast that an empty slot is available (if needed)
+      // Signal or broadcast that an empty slot is available (if needed)
+      notifyAll();
 
-          return super.get();
+      // Leave mutual exclusion done with synchronized -> releasing the lock
+      return super.get();
+   }
 
-          // Leave mutual exclusion
-    }
+   // Insert an element into buffer. If the attempted operation is
+   // not possible immedidately, the method call blocks until it is.
+   synchronized boolean put(Object value) {
+      // Enter mutual exclusion done with synchronized -> taking the lock
 
-    // Insert an element into buffer. If the attempted operation is
-    // not possible immedidately, the method call blocks until it is.
-    boolean put(Object value) {
-        
-       // Enter mutual exclusion
-            
-          // Wait until there is a empty slot available.
+      // Wait until there is a empty slot available.
+      while (size == maxSize) {
+         try {
+            wait();
+         } catch (InterruptedException e) {
+            e.printStackTrace();
+         }
+      }
 
-          // Signal or broadcast that a full slot is available (if needed)
+      // Signal or broadcast that a full slot is available (if needed)
+      notifyAll();
 
-          super.put(value);
+      super.put(value);
 
-          // Leave mutual exclusion
-       return true;
-    }
+      // Leave mutual exclusion done with synchronized -> releasing the lock
+      return true;
+   }
 
-    // Extract an element from buffer. If the attempted operation is not
-    // possible immedidately, return NULL. Otherwise, return the element.
-    Object remove() {
+   // Extract an element from buffer. If the attempted operation is not
+   // possible immedidately, return NULL. Otherwise, return the element.
+   synchronized Object remove() {
+      // Enter mutual exclusion done with synchronized -> taking the lock
 
-       // Enter mutual exclusion
-            
-          // Signal or broadcast that an empty slot is available (if needed)
+      // Check if a full slot is available
+      if (size == 0) {
+         // Leave mutual exclusion done with synchronized -> releasing the lock
+         return null;
+      } else {
+         // Signal or broadcast that an empty slot is available (if needed)
+         notifyAll();
+         // Leave mutual exclusion done with synchronized -> releasing the lock
+         return super.get();
+      }
+   }
 
-          return super.get();
+   // Insert an element into buffer. If the attempted operation is
+   // not possible immedidately, return 0. Otherwise, return 1.
+   synchronized boolean add(Object value) {
+      // Enter mutual exclusion done with synchronized -> taking the lock
+      boolean done;
 
-          // Leave mutual exclusion
-    }
+      // Check if a empty slot is available
+      if (size == maxSize) {
+         // Leave mutual exclusion done with synchronized -> releasing the lock
+         return false;
+      } else {
+         // Signal or broadcast that a full slot is available (if needed)
+         notifyAll();
+         super.put(value);
+         // Leave mutual exclusion done with synchronized -> releasing the lock
+         return true;
+      }
+   }
 
-    // Insert an element into buffer. If the attempted operation is
-    // not possible immedidately, return 0. Otherwise, return 1.
-    boolean add(Object value) {
-       boolean done;
+   // Extract an element from buffer. If the attempted operation is not
+   // possible immedidately, the method call blocks until it is, but
+   // waits no longer than the given deadline. Return the element if
+   // successful. Otherwise, return NULL.
+   synchronized Object poll(long deadline) {
+      // Enter mutual exclusion done with synchronized -> taking the lock
+      long timeout;
 
-       // Enter mutual exclusion
-            
-          // Signal or broadcast that a full slot is available (if needed)
+      // Wait until a full slot is available but wait
+      // no longer than the given deadline
+      while (size == 0) {
+         timeout = deadline - System.currentTimeMillis();
+         if (timeout <= 0) {
+            // Leave mutual exclusion done with synchronized -> releasing the lock
+            return null;
+         }
+         try {
+            wait(timeout);
+         } catch (InterruptedException e) {
+            e.printStackTrace();
+         }
+      }
 
-          return super.put(value);
+      if (size == 0)
+         return null;
 
-          // Leave mutual exclusion
-    }
+      // Signal or broadcast that an full slot is available (if needed)
+      notifyAll();
 
-    // Extract an element from buffer. If the attempted operation is not
-    // possible immedidately, the method call blocks until it is, but
-    // waits no longer than the given deadline. Return the element if
-    // successful. Otherwise, return NULL.
-    Object poll(long deadline) {
-       long    timeout;
+      // Leave mutual exclusion done with synchronized -> releasing the lock
+      return super.get();
+   }
 
-       // Enter mutual exclusion
+   // Insert an element into buffer. If the attempted operation is not
+   // possible immedidately, the method call blocks until it is, but
+   // waits no longer than the given deadline. Return 0 if not
+   // successful. Otherwise, return 1.
+   synchronized boolean offer(Object value, long deadline) {
+      // Enter mutual exclusion done with synchronized -> taking the lock
+      long timeout;
 
-          // Wait until a full slot is available but wait
-          // no longer than the given deadline
-    
-          if (size == 0) return null;
+      // Wait until a empty slot is available but wait
+      // no longer than the given deadline
+      while (size == maxSize) {
+         timeout = deadline - System.currentTimeMillis();
+         if (timeout <= 0) {
+            // Leave mutual exclusion done with synchronized -> releasing the lock
+            return false;
+         }
+         try {
+            wait(timeout);
+         } catch (InterruptedException e) {
+            e.printStackTrace();
+         }
+      }
 
-          // Signal or broadcast that an full slot is available (if needed)
+      if (size == maxSize)
+         return false;
 
-          return super.get();
+      // Signal or broadcast that an empty slot is available (if needed)
+      notifyAll();
 
-          // Leave mutual exclusion 
-    }
+      super.put(value);
 
-    // Insert an element into buffer. If the attempted operation is not
-    // possible immedidately, the method call blocks until it is, but
-    // waits no longer than the given deadline. Return 0 if not
-    // successful. Otherwise, return 1.
-    boolean offer(Object value, long deadline) {
-       long    timeout;
-
-       // Enter mutual exclusion
-
-          // Wait until a empty slot is available but wait
-          // no longer than the given deadline
-    
-          if (size == maxSize) return false;
-
-          // Signal or broadcast that an empty slot is available (if needed)
-
-          super.put(value);
-
-          // Leave mutual exclusion
-       return true;
-    }
+      // Leave mutual exclusion done with synchronized -> releasing the lock
+      return true;
+   }
 }
